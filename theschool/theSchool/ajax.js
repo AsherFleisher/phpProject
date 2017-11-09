3b18f0d22c41abcb97ed6f1a838ac9daa674b7e2
Asher Fleisher <asherfleisherr@gmail.com>
2017-11-09 16:23:40 +0200

    Add files via upload

--- theschool/theSchool/ajax.js
+++ theschool/theSchool/ajax.js
@@ -41,7 +41,7 @@ function ajax(a) {
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             document.getElementById(ajax).innerHTML = this.responseText;
         }
     };
--- theschool/theSchool/course.php
+++ theschool/theSchool/course.php
@@ -1,29 +1,27 @@
 <?php
 class course{
             
-        function allCourse()
-        {
-                require "DAL.php";
-                $statement = $pdo->query("SELECT * FROM course");
-                
-                foreach ($statement as $row)
-                {          
-                        echo  "<button class='course' name='course' onclick='ajax(this)'>" . $row['name'] . "</button><br/>";
-                } 
-        }
+                        function allCourse()
+                        {
+                                require "DAL.php";
+                                $statement = $pdo->query("SELECT * FROM course");
+                                
+                                foreach ($statement as $row)
+                                {          
+                                        echo  "<button class='course' name='course' onclick='ajax(this)'>" . $row['name'] . "</button><br/>";
+                                } 
+                        }
 
-        function chooseCourse()
-     {
-        $courseName = $_SESSION['courseName'];
-        require "DAL.php";
-        $statement = $pdo->query("SELECT * FROM course");
-              
-               foreach ($statement as $row)
-               {          if($row['name'] ===  $courseName)
-                       echo   $row['name'] . $row['description'] . "<br/>";
-               }
-     }
-       
-            
+                        function chooseCourse()
+                        {
+                                $courseName = $_SESSION['courseName'];
+                                require "DAL.php";
+                                $statement = $pdo->query("SELECT * FROM course");
+                                
+                                foreach ($statement as $row)
+                                {          if($row['name'] ===  $courseName)
+                                        echo   $row['name'] . $row['description'] . "<br/>";
+                                }
+                        }     
         }
 ?>
